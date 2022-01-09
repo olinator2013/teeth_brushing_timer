85d490a296c21e6749aec5455dae04569c60d85d
olinator2013 <93063569+olinator2013@users.noreply.github.com>
2022-01-09 15:56:55 -0500

    Added pause button

--- main.ts
+++ main.ts
@@ -1,12 +1,21 @@
+let pause2 = false
 let max_counter = 0
 let counter = 0
 input.onButtonPressed(Button.A, function () {
-    max_counter = 10
+    pause2 = false
+    max_counter = 19
     counter = 0
     while (counter < max_counter) {
-        basic.pause(1000)
-        counter += 1
-        basic.showNumber(counter)
+        if (pause2 == false) {
+            basic.pause(1000)
+            counter += 1
+            basic.showNumber(counter)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
     }
     basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    pause2 = !(pause2)
 })

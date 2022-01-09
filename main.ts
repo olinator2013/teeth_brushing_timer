e84a50fdb35956edb5dd71d2be4a6232aa22922d
olinator2013 <93063569+olinator2013@users.noreply.github.com>
2022-01-09 15:45:45 -0500

    First version with counter

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let max_counter = 0
+let counter = 0
+input.onButtonPressed(Button.A, function () {
+    max_counter = 10
+    counter = 0
+    while (counter < max_counter) {
+        basic.pause(1000)
+        counter += 1
+        basic.showNumber(counter)
+    }
+    basic.showIcon(IconNames.Happy)
+})
